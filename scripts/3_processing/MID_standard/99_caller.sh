21d3ddd0e06617556311e301a940149addeb1699
Kat Seitz <katharina.seitz@northwestern.edu>
2024-09-12 03:28:44 -0500

    syncing after merging scripts

--- scripts/3_processing/MID_standard/99_caller.sh
+++ scripts/3_processing/MID_standard/99_caller.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=15GB
+#SBATCH --mem 4GB
 #SBATCH --job-name="FoundMIDfirstlevel" 
-#SBATCH --time=20:00:00
+#SBATCH --time=10:00:00
 #SBATCH --mail-user=katharina.seitz@northwestern.edu 
 
 python MID_first_levels.py
